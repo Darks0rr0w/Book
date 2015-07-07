a0e5b95e5d8fef2d077f6233697857d2f7bad47b
Darks0rr0w <z1msada@gmail.com>
2015-07-07 03:46:20 +0300

    update function

--- db.php
+++ db.php
@@ -14,24 +14,24 @@ class Entry extends DataBase
     private $status;
     private $date;
 
-     function __construct()
-     {
-         $this->dbh = new PDO('mysql:host=localhost;dbname=Book', $this->user, $this->password);
-     }
+    function __construct()
+    {
+        parent::__construct();
+    }
 
     public function save()
     {
         $this->content = mysql_real_escape_string($this->content);
         $sql = "INSERT INTO entry (content,date,status) VALUES (':content', ':date', ':status')";
         $q = $this->dbh->prepare($sql);
-        $q->bindParam(':content', $this->content );
+        $q->bindParam(':content', $this->content);
         $q->bindParam(':date', $this->date);
         $q->bindParam(':status', $this->status);
         $q->execute();
     }
 
 
-    public  function find($id)
+    public function find($id)
     {
         $sql = "SELECT * FROM entry WHERE id = ':id'";
         $q = $this->dbh->prepare($sql);
@@ -51,6 +51,22 @@ class Entry extends DataBase
         $result = $q->fetchAll(PDO::FETCH_ASSOC);
 
         return $result;
+    }
+
+    public  function delete($id)
+    {
+        $sql = "DELETE FROM entry WHERE id = :id";
+        $q = $this->dbh->prepare($sql);
+        $q->bindParam('id', $id, PDO::PARAM_INT);
+        $q->execute();
+    }
+
+    public function update($id)
+    {
+        $sql = "UPDATE entry SET status = 1 WHERE id = :$id";
+        $q = $this->dbh->prepare($sql);
+        $q->bindParam('id',$id, PDO::PARAM_INT);
+        $q->execute();
     }
 
     public function close()
--- index.php
+++ index.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: z1m
- * Date: 01.07.2015
- * Time: 20:50
- */
