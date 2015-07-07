295a5fcedfe61ccb0eb0c1e0f3531a6132cadd56
Darks0rr0w <z1msada@gmail.com>
2015-07-07 04:06:58 +0300

    pagination

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+require_once('db.php');
+$test = new Entry();
+?>
+<!DOCTYPE HTML>
+
+
+<html lang="en">
+
+
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+    <title>Title Goes Here</title>
+
+</head>
+
+<body>
+
+<p>Complains and suggestions</p>
+
+<?php $test->findAll(); ?>
+
+</body>
+
+
+
+</html>
+
+
