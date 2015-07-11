ae13358731fcec42182a7e2e710484ca963eb6e1
Darks0rr0w <z1msada@gmail.com>
2015-07-11 10:07:50 +0300

    navbar

--- index.php
+++ index.php
@@ -42,9 +42,8 @@ if(isset($_POST['update']) && !empty($_POST['checkbox']))
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <?
-                    <li><a href="login.php">Admin</a></li>
-                    <
+                    <li><a href="login.php">Login as admin</a></li>
+                    <li><a href="index.php">Logout</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
